f74785529a2c12c6f4ec6e056ae913678f7a6f15
Vaibhav Gorana <vaibhavgorana80@gmail.com>
2025-08-20 13:36:44 +0530

    Phase 2 - Added new feature and working now

---
+++ allsong.js
@@ -0,0 +1,22 @@
+
+
+const AllSONGS = [
+
+  { id:1, title:'Afreen Afreen', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/afreen.jpg', tags:['love','legendary'], plays: 15234, added:'2024-09-03' },
+  { id:2, title:'Dukh Raj Raj Data Nu', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/movie.mp4', cover:'asset/dukhraj.jpg', tags:['spiritual','live'], plays: 20291, added:'2023-12-10' },
+  { id:3, title:'Ye Jo Halka Halka Suroor Hai', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/yehjohalka.jpg', tags:['love','sufi'], plays: 51290, added:'2022-05-14' },
+  { id:4, title:'Sanson Ki Mala Pe', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/movie.mp4', cover:'asset/sansoki.jpg', tags:['spiritual','devotional'], plays: 34100, added:'2021-02-18' },
+  { id:5, title:'Dum Mast Qalandar', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/dummastqal.jpg', tags:['sufi','celebration'], plays: 78020, added:'2019-10-11' },
+  { id:6, title:'Mera Piya Ghar Aya', artist:'Nusrat Fateh Ali Khan', file:'https://mega.nz/embed/zc0UiSRb#u8ouDD4iYPmV2MLMisbwQDB6aG9fmHfCHfvev_6BbGA', cover:'asset/merapiya.jpg', tags:['celebration','legendary'], plays: 26340, added:'2020-08-01' },
+  { id:7, title:'Kinna Sona Tenu', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/kinhasonha.jpg', tags:['love', 'celebration'], plays: 11800, added:'2024-01-20' },
+  { id:8, title:'Man Kunto Maula', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/movie.mp4', cover:'asset/mankunto.jpg', tags:['sufi','live'], plays: 45210, added:'2018-06-09' },
+  { id:9, title:'Jaani Door Gaye', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/movie.mp4', cover:'asset/janidoorgaye.jpg', tags:['sufi','love','legendary'], plays: 45211, added:'2018-06-09' },
+  { id:10, title:'Tum Ek Gorakh Dhanda Ho', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/TumEkGorakh.jpg', tags:['sufi','celebration','power','legendary'], plays: 78020, added:'2019-10-11' },
+  { id:11, title:'Iss Karam Ka Karu Shukr Kese Ada', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/isskaramka.jpg', tags:['sufi','celebration','legendary'], plays: 78020, added:'2019-10-11' },
+  { id:12, title:'Allah Hoo', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/allahhoo.jpg', tags:['sufi','devotional'], plays: 78020, added:'2019-10-11' },
+  { id:13, title:'Shah-e-Mardan-e-Ali', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'asset/shahemardan.jpg', tags:['sufi','power'], plays: 78020, added:'2019-10-11' },
+  { id:14, title:'Akhiyan Udeek Diyan', artist:'Nusrat Fateh Ali Khan', file:'https://www.w3schools.com/html/mov_bbb.mp4', cover:'lega/perform.jpg', tags:['love','celebration'], plays: 78020, added:'2019-10-11' },
+  
+];
+// Build lookup table: ID -> Song object
+
---
+++ playlist.js
@@ -0,0 +1,74 @@
+const MOODS = [
+      {
+        id: '1',
+        title: 'Love & Romance',
+        desc: 'Tender qawwalis and romantic ghazals — the language of ishq.',
+        thumb: '', // optional img path
+        songs: [1,3,7,9,14 ],
+        created: "2025-01-10"
+      },
+      {
+        id: '2',
+        title: 'Ruhani (Devotion)',
+        desc: 'Spiritual qawwalis that open the heart to the Divine.',
+        songs: [ 4,12],
+         created: "2025-01-10"
+      },
+      {
+        id: '3',
+        title: 'Mehfil (Live)',
+        desc: 'Long live performances, audience interaction, soulful alaaps.',
+        songs: [ 8,3 ],
+         created: "2025-01-10"
+      },
+      {
+        id: '4',
+        title: 'Mauj (Celebration)',
+        desc: 'Festive qawwalis for joyous gatherings.',
+        songs: [10,5,6,14  ],
+         created: "2025-01-10"
+      },
+      {
+        id: '5',
+        title: 'Melancholy & Reflection',
+        desc: 'Slower pieces — introspective and tender.',
+        songs: [ ],
+         created: "2025-01-10"
+      },
+      {
+        id: '6',
+        title: 'Masterpieces',
+        desc: 'No one can remake or even copy , impossible masterpieces ',
+        songs: [ 3,4,5,9,10,11 ],
+         created: "2025-01-10"
+      },
+      {
+        id: '7',
+        title: 'Power & Passion',
+        desc: 'High energy qawwalis, intense vocals and rhythmic drive.',
+        songs: [ 10,13      ],
+         created: "2025-01-10"
+      },
+      {
+        id: '8',
+        title: 'Top Qawwalis',
+        desc: 'Most liked qawwalis, intense vocals and rhythmic drive.',
+        songs: [  10,3,13,4,5,9,11    ],
+         created: "2025-01-10"
+      },
+      {
+        id: '9',
+        title: 'Under-rated Masterpiece ',
+        desc: 'More deep and underrated ghazals and qawwalis',
+        songs: [  2  ,8 ],
+         created: "2025-01-10"
+      },
+      {
+        id: '10',
+        title: 'Interviews',
+        desc: 'When the legend spoke about himself. Listen & Know about the legends in his own words',
+        songs: [    ],
+         created: "2025-01-10"
+      }
+];
+
